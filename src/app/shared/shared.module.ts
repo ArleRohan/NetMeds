55d95379dca6e32a1e2b8d42a8d5749898705e12
shantanu0708 <shantanupatil6172@gmail.com>
2025-07-25 13:38:17 +0530

    health library updated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -19,6 +19,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { EyesComponent } from './components/Beauty/make-up/eyes/eyes.component';
 import { LoginComponent } from '../auth/login/login.component';
 import { FormsModule } from '@angular/forms';
+import { BlogsComponent } from './components/Health-Center/health-library/blogs/blogs.component';
 
 
 
@@ -26,7 +27,8 @@ const routes: Routes = [
   {path:"BuyAgain", component:BuyAgainComponent},
   {path:"OrderOnline",component:OrderOnlineComponent},
   {path:"cancer",component:CancerComponent},
-  {path:"signin", component:LoginComponent}
+  {path:"signin", component:LoginComponent},
+  {path: "HealthLibrary", component: HealthLibraryComponent}
 ];
 
 @NgModule({
@@ -46,7 +48,8 @@ const routes: Routes = [
     HealthLibraryComponent,
     CancerComponent,
     CoronaAwarenessComponent,
-    EyesComponent
+    EyesComponent,
+    BlogsComponent
   ],
   imports: [
     RouterModule.forChild(routes),
