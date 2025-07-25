46b92c4a63d8ac8c41b21c2166cb2e34930a0849
Komal Shewatkar <komalshewatkar60@gmail.com>
2025-07-25 14:19:30 +0530

    merging

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -9,8 +9,8 @@ import { CustomPipe } from '../shared/pipes/custom.pipe';
 const routes: Routes = [
   { path: "productList/:keyName", component: ProductListComponent },
   { path: "productDetails", component: ProductDetailsComponent },
-  {path:"productList/:eyes",component:EyesComponent}
-  { path: 'product-details/:type/:id', component: ProductDetailsComponent }
+  {path:"productList/:eyes",component:EyesComponent},
+  { path: "product-details/:type/:id", component: ProductDetailsComponent }
 ];
 
 @NgModule({
